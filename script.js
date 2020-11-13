9e4684730205acf4bae551e0aea3e1bb8cc36f4d
arjunsingh <arjunksingh1027@gmail.com>
2020-11-13 15:43:06 -0700

    added js for new time divs and color changes

--- script.js
+++ script.js
@@ -17,18 +17,43 @@ var workday = [
 ];
 
 
-// change colors on rows
-function rowColor(time) {
-    var planNow = moment(now, "H A");
-    var planEntry = moment(time, "H A");
-    if (planNow.isBefore(planEntry) === true) {
-        return "future";
-    } else if (planNow.isAfter(planEntry) === true) {
-        return "past";
-    } else {
-        return "present";
-    }
-}
+// counting up with each new time block div
+let counter = 1;
+for (const property in workday) {
+    let textEntry = "#text-entry" + counter;
+    $(textEntry).text(workday[property]);
+    let timeId = "#hour" + counter;
+    let presentHour = moment().hour();
+    let timeText = $(timeId).text();
+    let timeNum = hourNumberFromString(timeText);
+    if (timeNum < presentHour) {
+        $(textEntry).addClass("past");
+    } else if (timeNum > presentHour) {
+        $(textEntry).addClass("future");
+    } else { $(textEntry).addClass("present") }
+    counter++;
+};
+
+
+
+
+
+
+
+// if current hour, red 
+// if past, greyed out
+// if future, green
+// function rowColor(time) {
+//     var planNow = moment(now, "H A");
+//     var planEntry = moment(time, "H A");
+//     if (planNow.isBefore(planEntry) === true) {
+//         return "future";
+//     } else if (planNow.isAfter(planEntry) === true) {
+//         return "past";
+//     } else {
+//         return "present";
+//     }
+// }
 
 // save user input
 $(".saveBtn").on("click", function () {
