3afa04255ea8ebf7a63293dd08355bf2973e7cca
arjunsingh <arjunksingh1027@gmail.com>
2020-11-13 18:12:58 -0700

    added coloring to timeblocks need to fix local storage

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ var now = moment().format("H A");
 // today's date
 $("#currentDay").text(today);
 
-var workday = [
-    { time: "0900", event: "" },
-    { time: "1000", event: "" },
-    { time: "1100", event: "" },
-    { time: "1200", event: "" },
-    { time: "1300", event: "" },
-    { time: "1400", event: "" },
-    { time: "1500", event: "" },
-    { time: "1600", event: "" },
-    { time: "1700", event: "" },
-];
-
+let workday = {
+    "0900": "",
+    "1000": "",
+    "1100": "",
+    "1200": "",
+    "1300": "",
+    "1400": "",
+    "1500": "",
+    "1600": "",
+    "1700": "",
+};
 
 // counting up with each new time block div
 let counter = 1;
@@ -26,6 +25,8 @@ for (const property in workday) {
     let presentHour = moment().hour();
     let timeText = $(timeId).text();
     let timeNum = hourNumberFromString(timeText);
+
+    // if statement to change txt entry of div color
     if (timeNum < presentHour) {
         $(textEntry).addClass("past");
     } else if (timeNum > presentHour) {
@@ -34,42 +35,57 @@ for (const property in workday) {
     counter++;
 };
 
+// button click event to save into local storage
+$("button").click(function () {
+    value = $(this).siblings("textarea").val();
+    hourString = $(this).siblings("div").text();
 
-
-
-
-
-
-// if current hour, red 
-// if past, greyed out
-// if future, green
-// function rowColor(time) {
-//     var planNow = moment(now, "H A");
-//     var planEntry = moment(time, "H A");
-//     if (planNow.isBefore(planEntry) === true) {
-//         return "future";
-//     } else if (planNow.isAfter(planEntry) === true) {
-//         return "past";
-//     } else {
-//         return "present";
-//     }
-// }
-
-// save user input
-$(".saveBtn").on("click", function () {
-    var blockID = parseInt(
-        $(this)
-            .closest(".time-block")
-            .attr("id")
-    );
-    var userEntry = $.trim(
-        $(this)
-            .parent()
-            .siblings("textarea")
-            .val()
-    );
-    planWorkday[blockID].event = userEntry;
+    saveSchedule(hourString, value);
 });
 
-// local storage
-localStorage.setItem("workDay", JSON.stringify(planWorkday));
+function hourNumberFromString(hourString) {
+    switch (hourString) {
+        case "0900": return 9;
+        case "1000": return 10;
+        case "1100": return 11;
+        case "1200": return 12;
+        case "1300": return 13;
+        case "1400": return 14;
+        case "1500": return 15;
+        case "1600": return 16;
+        case "1700": return 17;
+    }
+};
+
+function loadCorrectDataset() {
+    result = localStorage.getItem("workday")
+    return (result ? result : workday);
+};
+
+function initLocalStorage() {
+    localStorage.setItem('workday', JSON.stringify(workday));
+};
+
+function saveLocalStorage(dayObj) {
+    localStorage.setItem("workday", JSON.stringify(dayObj));
+};
+
+function saveSchedule(hourString, val) {
+    if (!localStorage.getItem("workday")) {
+        initLocalStorage();
+    };
+
+
+    let workHours = JSON.parse(localStorage.getItem("workday"));
+    workHours[hourString] = val
+
+    saveLocalStorage(workHours);
+};
+
+function updateCalTasks(dayObject) {
+    $(".hour").each(function(index){
+        let result = $(this).children("div");
+        $(this).chilren("textarea").text(dayObject[result.text()]);
+    })
+};
+
